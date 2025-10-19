0d189067b5f76490107873e4d946eb84d1ab6bfc
Zengoozz <classicalegyptian@gmail.com>
2025-10-19 16:46:27 +0300

    add configurations totally and tested

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
 // Preparing point for the server application [Middlewares, Routes, etc.]
+
+import express, { type Request, type Response } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "✅ Express + TypeScript + ESM working fine!" });
+});
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,14 @@
 // Initition point for the server application
+
+import http from "node:http";
+import app from "./app.js"; // ← لاحظ الامتداد .js لأنه ESM
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
